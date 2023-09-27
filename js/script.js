6ea73ba299bc7be0b30a85febb277ae723fa1a0c
lucasesc07 <lucasjuventud557rgmail.com>
2023-09-27 14:48:55 -0300

    fondo body+transis.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const colorInput = document.getElementById("color-input");
 const changeColorButton = document.getElementById("change-color-button");
+const bodyColorInput = document.getElementById("body-color-input");
+const changeBodyColorButton = document.getElementById("change-body-color-button");
 const container = document.querySelector(".container");
 
 changeColorButton.addEventListener("click", () => {
     const color = colorInput.value;
     container.style.backgroundColor = color;
+});
+
+changeBodyColorButton.addEventListener("click", () => {
+    const bodyColor = bodyColorInput.value;
+    document.body.style.backgroundColor = bodyColor;
 });
